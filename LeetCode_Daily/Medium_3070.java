e64b39fcd8a701f1d5c5c98bd6b7d8b74e9fbf55
Mohit <mohitgirase1228@gmail.com>
2026-03-19 23:40:36 +0530

    Solved LeetCode daily problem

---
+++ LeetCode_Daily/Medium_3070.java
@@ -0,0 +1,47 @@
+package LeetCode_Daily;
+
+/*
+3070. Count Submatrices with Top-Left Element and Sum Less Than k
+
+You are given a 0-indexed integer matrix grid and an integer k.
+
+Return the number of submatrices that contain the top-left element of the grid, and have a sum less than or equal to k.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[7,6,3],[6,6,1]], k = 18
+Output: 4
+Explanation: There are only 4 submatrices, shown in the image above, that contain the top-left element of grid, and have a sum less than or equal to 18.
+Example 2:
+
+
+Input: grid = [[7,2,9],[1,5,0],[2,6,6]], k = 20
+Output: 6
+Explanation: There are only 6 submatrices, shown in the image above, that contain the top-left element of grid, and have a sum less than or equal to 20.
+ 
+
+Constraints:
+
+m == grid.length 
+n == grid[i].length
+1 <= n, m <= 1000 
+0 <= grid[i][j] <= 1000
+1 <= k <= 109
+*/
+public class Medium_3070 {
+    public int countSubmatrices(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        int ans = 0;
+        int[][] px = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                px[i+1][j+1] = grid[i][j] + px[i][j+1] + px[i+1][j] - px[i][j];
+                if (px[i+1][j+1] <= k) ans++;
+            }
+        }
+        return ans;
+    }
+}
