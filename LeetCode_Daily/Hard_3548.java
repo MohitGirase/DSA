6d63ce81bfce0891e52fa12922b610731d00e08a
Mohit <mohitgirase1228@gmail.com>
2026-03-27 00:34:57 +0530

    Solved LeetCode Daily problem

---
+++ LeetCode_Daily/Hard_3548.java
@@ -0,0 +1,164 @@
+package LeetCode_Daily;
+
+/*
+3548. Equal Sum Grid Partition II
+
+You are given an m x n matrix grid of positive integers. Your task is to determine if it is possible to make either one horizontal or one vertical cut on the grid such that:
+
+Each of the two resulting sections formed by the cut is non-empty.
+The sum of elements in both sections is equal, or can be made equal by discounting at most one single cell in total (from either section).
+If a cell is discounted, the rest of the section must remain connected.
+Return true if such a partition exists; otherwise, return false.
+
+Note: A section is connected if every cell in it can be reached from any other cell by moving up, down, left, or right through other cells in the section.
+
+ 
+
+Example 1:
+
+Input: grid = [[1,4],[2,3]]
+
+Output: true
+
+Explanation:
+
+
+
+A horizontal cut after the first row gives sums 1 + 4 = 5 and 2 + 3 = 5, which are equal. Thus, the answer is true.
+Example 2:
+
+Input: grid = [[1,2],[3,4]]
+
+Output: true
+
+Explanation:
+
+
+
+A vertical cut after the first column gives sums 1 + 3 = 4 and 2 + 4 = 6.
+By discounting 2 from the right section (6 - 2 = 4), both sections have equal sums and remain connected. Thus, the answer is true.
+Example 3:
+
+Input: grid = [[1,2,4],[2,3,5]]
+
+Output: false
+
+Explanation:
+
+
+
+A horizontal cut after the first row gives 1 + 2 + 4 = 7 and 2 + 3 + 5 = 10.
+By discounting 3 from the bottom section (10 - 3 = 7), both sections have equal sums, but they do not remain connected as it splits the bottom section into two parts ([2] and [5]). Thus, the answer is false.
+Example 4:
+
+Input: grid = [[4,1,8],[3,2,6]]
+
+Output: false
+
+Explanation:
+
+No valid cut exists, so the answer is false.
+
+ 
+
+Constraints:
+
+1 <= m == grid.length <= 105
+1 <= n == grid[i].length <= 105
+2 <= m * n <= 105
+1 <= grid[i][j] <= 105
+*/
+public class Hard_3548 {
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        long total = 0;
+
+        int[] bottom = new int[100001];
+        int[] top = new int[100001];
+        int[] left = new int[100001];
+        int[] right = new int[100001];
+
+        for (int[] row : grid) {
+            for (int x : row) {
+                total += x;
+                bottom[x]++;
+                right[x]++;
+            }
+        }
+
+        long sumTop = 0;
+
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = grid[i][j];
+                sumTop += val;
+
+                top[val]++;
+                bottom[val]--;
+            }
+
+            long sumBottom = total - sumTop;
+
+            if (sumTop == sumBottom) return true;
+
+            long diff = Math.abs(sumTop - sumBottom);
+
+            if (diff <= 100000) {
+                if (sumTop > sumBottom) {
+                    if (check(top, grid, 0, i, 0, n - 1, diff)) return true;
+                } else {
+                    if (check(bottom, grid, i + 1, m - 1, 0, n - 1, diff)) return true;
+                }
+            }
+        }
+
+        long sumLeft = 0;
+
+        // Vertical cuts
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < m; i++) {
+                int val = grid[i][j];
+                sumLeft += val;
+
+                left[val]++;
+                right[val]--;
+            }
+
+            long sumRight = total - sumLeft;
+
+            if (sumLeft == sumRight) return true;
+
+            long diff = Math.abs(sumLeft - sumRight);
+
+            if (diff <= 100000) {
+                if (sumLeft > sumRight) {
+                    if (check(left, grid, 0, m - 1, 0, j, diff)) return true;
+                } else {
+                    if (check(right, grid, 0, m - 1, j + 1, n - 1, diff)) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check(int[] freq, int[][] grid,
+                          int r1, int r2, int c1, int c2, long diff) {
+
+        int rows = r2 - r1 + 1;
+        int cols = c2 - c1 + 1;
+
+        if (rows * cols == 1) return false;
+
+        if (rows == 1) {
+            return grid[r1][c1] == diff || grid[r1][c2] == diff;
+        }
+
+        if (cols == 1) {
+            return grid[r1][c1] == diff || grid[r2][c1] == diff;
+        }
+
+        return freq[(int)diff] > 0;
+    }
+}
